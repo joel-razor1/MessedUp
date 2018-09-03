6c93d95bc458dfe423bafbe4603be1d567a54bb6
notacoolcoder <notacoolcoder@gmail.com>
2018-09-03 17:11:31 +0530

    404 added route need to be fix major update

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -4,17 +4,19 @@ import logo from "../../Res/logo_text_w.png";
 import { Switch, Button, Icon, message } from "antd";
 import { Link } from "react-router-dom";
 import { auth } from "../../util/config";
-
+import joel from "../../Res/joel.jpg"
+import cj from "../../Res/cj.jpg"
+import sn from "../../Res/sn.jpg"
 const About = () => {
   return (
     <div>
-      <hr style={{ width: "350px", color: "#e6e6e6", opacity: "0.5" }} />
+      <hr style={{ width: "90%", color: "#e6e6e6", opacity: "0.5" }} />
       <div>
         <div>version</div>
         <div>2.0</div>
       </div>
 
-      <hr style={{ width: "350px", color: "#e6e6e6", opacity: "0.5" }} />
+      <hr style={{ width: "90%", color: "#e6e6e6", opacity: "0.5" }} />
       <h4 style={{ color: "white" }}>Follow Us At</h4>
       <div style={{ fontSize: "30px" }}>
         <a href="https://github.com/akshaycj" style={{ color: "white" }}>
@@ -65,11 +67,11 @@ export default class extends Component {
   onSignout = () => {
     auth
       .signOut()
-      .then(function() {
+      .then(function () {
         // Sign-out successful.
         message.success("This is a message of success");
       })
-      .catch(function(error) {
+      .catch(function (error) {
         // An error happened.
         message.error("This is a message of error");
       });
@@ -83,7 +85,7 @@ export default class extends Component {
       <div className="settings-main">
         <h3 style={{ color: "white", fontSize: 22 }}>Settings</h3>
         <div style={{ display: "flex", justifyContent: "center" }} />
-        <hr style={{ width: "340px", color: "#e6e6e6", opacity: "0.5" }} />
+        <hr style={{ width: "90%", color: "#e6e6e6", opacity: "0.5" }} />
         <div className="settings-sub">
           <h3 style={{ color: "#fff" }}>Food Preference</h3>{" "}
           <Switch
@@ -93,13 +95,13 @@ export default class extends Component {
             onChange={this.onFood}
           />
         </div>
-        <hr style={{ width: "350px", color: "#fff", opacity: "0.5" }} />
+        <hr style={{ width: "90%", color: "#fff", opacity: "0.5" }} />
         <div>
           <Link to="/">
             <h3 style={{ color: "#fff", cursor: "pointer" }}>Profile</h3>
           </Link>
         </div>
-        <hr style={{ width: "350px", color: "#e6e6e6", opacity: "0.5" }} />
+        <hr style={{ width: "90%", color: "#e6e6e6", opacity: "0.5" }} />
         <div>
           <h3
             style={{ color: "#fff", cursor: "pointer" }}
@@ -107,14 +109,49 @@ export default class extends Component {
           >
             About
           </h3>
-          {this.state.display ? (
-            <h5 style={{ color: "white", padding: "10px" }}>
-              Messed Up is an app that helps in the mess management effectively
-            </h5>
-          ) : null}
+          {this.state.display ?
+
+            <div>
+              <h5 style={{ color: "white", padding: "10px" }}>
+                Messed Up is an app that helps in the mess management effectively
+          </h5>
+              <div style={{display:"flex",flexDirection:"column",justifyContent:"center"}}>
+                <h3 style={{color:"white"}}>The Team</h3>
+                <div style={{ display: "flex", marginTop: "-35px" }}>
+                  <div class="tc pa4" >
+                    <img src={cj} class="br-100  h3 w3" alt="avatar" />
+                   <h6 style={{color:"white"}}>Akshay CJ</h6> 
+                  </div>
+                  <div class="tc pa4">
+                    <img src="http://tachyons.io/img/logo.jpg" class="br-100  h3 w3" alt="avatar" />
+                  <h6 style={{color:"white"}}>Adhil Juvane</h6>
+                  </div>
+                  <div class="tc pa4">
+                    <img src={joel} class="br-100  h3 w3" alt="avatar" />
+                    <h6 style={{color:"white"}}>Joel Johnson</h6>
+                  </div>
+                </div>
+                <div style={{ display: "flex", marginTop: "-15%",justifyContent:"center" }}>
+                  <div class="tc pa4">
+                    <img src="http://tachyons.io/img/logo.jpg" class="br-100  h3 w3" alt="avatar" />
+                    <h6 style={{color:"white"}}>Akhil A M</h6>
+                  </div>
+                  <div class="tc pa4">
+                    <img src={sn}class="br-100  h3 w3" alt="avatar" />
+                    <h6 style={{color:"white"}}>Sachin Nair</h6>
+                  </div>
+                  <div class="tc pa4">
+                    <img src="http://tachyons.io/img/logo.jpg" class="br-100  h3 w3" alt="avatar" />
+                    <h6 style={{color:"white"}}>Karthik sankar</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            : null}
         </div>
         <About />
-        <hr style={{ width: "350px", color: "#e6e6e6", opacity: "0.5" }} />
+        <hr style={{ width: "90%", color: "#e6e6e6", opacity: "0.5" }} />
         <div>
           <Button style={{ borderRadius: "10px" }} onClick={this.onSignout}>
             Sign Out
--- src/index.js
+++ src/index.js
@@ -11,21 +11,33 @@ import Auth from "./components/Auth";
 import Home from "./components/Home";
 import Main from "./components/Main";
 import Settings from "./components/Settings";
+import error from "./components/Error404"
+
+
+
 
 ReactDOM.render(
   <MuiThemeProvider>
     <Router>
       <Switch>
+
         <Route path="/" exact component={Auth} />
+
+        
+        
+
         <App>
           <Main>
             <Switch>
               <Route path="/home" exact component={Home} />
               <Route path="/settings" exact component={Settings} />
               <Route path="/usercuts" exact component={UserCuts} />
+              <Route component={error}/>
             </Switch>
           </Main>
+          
         </App>
+        
       </Switch>
     </Router>
   </MuiThemeProvider>,
